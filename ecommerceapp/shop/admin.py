e3ef456bac4cecb2ac32fbed1e63e14165f1b3ad
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-17 19:18:29 +0100

    Created and registered UserRole model and other models.

--- ecommerceapp/shop/admin.py
+++ ecommerceapp/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ProductCategory, Order, OrderItem
+from django.contrib.auth.models import User
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- ecommerceapp/users/models.py
+++ ecommerceapp/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+
+class UserRole(AbstractUser):
+    ROLE_CHOICES = [
+        ("admin", "Admin"),
+        ("seller", "Seller"),
+        ("customer", "Customer"),
+    ]
+    role = models.CharField(choices=ROLE_CHOICES, max_length=20)
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
