3dd83a0caa7220eac1fa88db33b11303b3ba77ed
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-17 20:22:32 +0100

    Added functionality of adding a product, its image and creating a thumbnail of an image

--- ecommerceapp/shop/serializers.py
+++ ecommerceapp/shop/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from .models import Product
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import uuid
+from copy import deepcopy
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -12,3 +17,51 @@ class ProductDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
+
+class ProductCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ("name", "description", "price", "category", "image")
+
+    def create(self, validated_data):
+        thumbnail_size = (200, 200)
+        image = validated_data.pop("image", None)
+
+        product = Product.objects.create(**validated_data)
+        if image:
+            image = Image.open(image)
+
+            # Kopiowanie obrazu przed zastosowaniem thumbnail
+            image_copy = deepcopy(image)
+            image_copy.thumbnail(thumbnail_size)
+
+            unique_id = uuid.uuid4().hex
+
+            # Zapis miniatury
+            thumbnail_io = BytesIO()
+            image_copy.save(thumbnail_io, format="JPEG")
+            thumbnail = InMemoryUploadedFile(
+                thumbnail_io,
+                None,
+                f"thumbnail_{unique_id}.jpg",
+                "image/jpeg",
+                thumbnail_io.tell(),
+                None,
+            )
+            product.thumbnail.save(f"thumbnail_{unique_id}.jpg", thumbnail)
+
+            # Zapis oryginalnego obrazu
+            image_io = BytesIO()
+            image.save(image_io, format="JPEG")
+            image = InMemoryUploadedFile(
+                image_io,
+                None,
+                f"image_{unique_id}.jpg",
+                "image/jpeg",
+                image_io.tell(),
+                None,
+            )
+            product.image.save(f"image_{unique_id}.jpg", image)
+
+        return product
--- ecommerceapp/shop/views.py
+++ ecommerceapp/shop/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics, permissions, filters
 from rest_framework.pagination import PageNumberPagination
 from .models import Product
-from .serializers import ProductListSerializer, ProductDetailsSerializer
+from .serializers import (
+    ProductListSerializer,
+    ProductDetailsSerializer,
+    ProductCreateSerializer,
+)
+from .permissions import IsSellerOrAdmin
+from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class ProductListView(generics.ListAPIView):
@@ -16,3 +22,15 @@ class ProductListView(generics.ListAPIView):
 class ProductDetailsView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductDetailsSerializer
+
+
+class ProductCreateView(generics.CreateAPIView):
+    serializer_class = ProductCreateSerializer
+    permission_classes = [IsSellerOrAdmin]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def perform_create(self, serializer):
+        serializer.save(
+            image=self.request.data.get("image"),
+            thumbnail=self.request.data.get("thumbnail"),
+        )
