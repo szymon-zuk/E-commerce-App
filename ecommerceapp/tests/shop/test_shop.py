2f27e3b247bc7e81ad8b4479f28ad5732eda367f
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-19 16:00:03 +0100

    added tests for ProductRetrieveUpdateDestroyView

--- ecommerceapp/tests/shop/test_shop.py
+++ ecommerceapp/tests/shop/test_shop.py
@@ -36,7 +36,7 @@ def test_product_detail_view(client, product):
 
 @pytest.mark.django_db
 def test_product_create_view(client, user_seller, product_category):
-    create_product_url = reverse("create-product")
+    url = reverse("create-product")
 
     payload = {
         "name": "New Product",
@@ -45,9 +45,37 @@ def test_product_create_view(client, user_seller, product_category):
         "category": product_category.id,
     }
 
-    response = client.post(create_product_url, data=payload)
+    response = client.post(url, data=payload)
     assert response.status_code == 201
     assert response.data["name"] == payload["name"]
     assert response.data["description"] == payload["description"]
     assert response.data["price"] == payload["price"]
     assert response.data["category"] == product_category.id
+
+
+@pytest.mark.django_db
+def test_product_update_view_method_get(client, product):
+    url = reverse("retrieve-update-delete-product", kwargs={"pk": product.id})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_product_update_view_method_put(client, product, product_category):
+    url = reverse("retrieve-update-delete-product", kwargs={"pk": product.id})
+    payload = {
+        "name": "New Product",
+        "description": "Product description",
+        "price": "29.99",
+        "category": product_category.id,
+    }
+    response = client.put(url, data=payload)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_product_update_view_method_delete(client, product):
+    url = reverse("retrieve-update-delete-product", kwargs={"pk": product.id})
+    response = client.delete(url)
+    assert response.status_code == 204
+    assert not Product.objects.filter(id=product.id).exists()
