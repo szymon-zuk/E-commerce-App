57f25be9f47e595fe7f6c83d2237136785b96359
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-19 11:00:24 +0100

    Added Dockerfile and pytest configuration

--- ecommerceapp/ecommerceapp/settings.py
+++ ecommerceapp/ecommerceapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY", default="django_insecure")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
