aecf2bdcf9f5343abf8cc02a7b9e279bb5877609
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-23 18:56:08 +0100

    Rollback

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn ecommerceapp.wsgi:application
--- ecommerceapp/ecommerceapp/settings.py
+++ ecommerceapp/ecommerceapp/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [
     "0.0.0.0",
     "localhost",
     "127.0.0.1",
-    "https://e-commerce-app-production-f404.up.railway.app/",
 ]
 
 
